5bd77c71d0c88151f80d09284ea570bf7b1166b2
emizap <emersoni@superiorcsgroup.com>
2021-11-09 14:35:53 +0100

    New File Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import HomePage from './HomePage';
 import Register from './Register';
+import Dashboard from './DashBoard';
 import { BrowserRouter, Route,Switch } from 'react-router-dom';
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
         <Switch>
             <Route path = '/' component={HomePage} exact/>
             <Route path = '/register' component={Register} />
+            <Route path = '/dashboard' component={Dashboard} />
         </Switch>
         </BrowserRouter>
   );
