430b52d82261629b26d96ab9c6729cea5070dafe
emizap <emersoni@superiorcsgroup.com>
2021-11-09 18:01:10 +0100

    updated

--- src/DashBoard.js
+++ src/DashBoard.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Dashboard.css';
+import logo2 from './logo2.svg';
+import clock from './clock.svg';
 
 const Dashboard = () =>{
     return(
         <div className="dashboardHome">
             <div className="SideBar">
-                mmmmmm
+                <div className="logo">
+                <img src={logo2} className="dashlogo" alt="logo" />
+                </div>
+            </div>
+            <div className="headerArea">
+                <div>
+                <p><img src={clock} className="clock" alt="clock" /><font className="headerAreaDir"><h4>Estimating</h4></font></p>
+                 </div>
             </div>
         </div>
     )
