dc2a602dcf6cd68a20d1fafc01d534f46276bc88
emizap <emersoni@superiorcsgroup.com>
2021-11-10 14:38:13 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import HomePage from './HomePage';
 import Register from './Register';
 import Dashboard from './DashBoard';
 import { BrowserRouter, Route,Switch } from 'react-router-dom';
+import Estimates from './Estimates';
 function App() {
   return (
     <BrowserRouter>
         <Switch>
             <Route path = '/' component={HomePage} exact/>
             <Route path = '/register' component={Register} />
+            <Route path = '/estimates-home' component={Estimates} />
             <Route path = '/dashboard' component={Dashboard} />
         </Switch>
         </BrowserRouter>
