84e21dbc87be89a86f58cf3222f23d2848a4452f
emizap <emersoni@superiorcsgroup.com>
2021-11-12 10:30:02 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Register from './Register';
 import Dashboard from './DashBoard';
 import { BrowserRouter, Route,Switch } from 'react-router-dom';
 import Estimates from './Estimates';
+import PreviewEstimate from './PreviewEstimate';
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +12,8 @@ function App() {
             <Route path = '/register' component={Register} />
             <Route path = '/estimates-home' component={Estimates} />
             <Route path = '/dashboard' component={Dashboard} />
+            <Route path = '/previewEstimates' component={PreviewEstimate} />
+
         </Switch>
         </BrowserRouter>
   );
---
+++ src/PreviewEstimate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PreviewEstimate = () =>{
+    return(
+        <div>
+            <h2>Preview Estimate</h2>
+        </div>
+    );
+}
+
+export default PreviewEstimate;
